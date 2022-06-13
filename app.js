cbaa0968984ee727ec148223bbb3ebcdb717e732
Yummy275 <htgonzalez94@yahoo.com>
2022-06-13 11:36:01 -0700

    mongo connect

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 
+//Set up mongoose connection
+var mongoDB =
+    'mongodb+srv://Yummy:Indeed@cluster0.gvyhh.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(
+    mongoDB,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (!err) console.log('MongoDB connection successful.');
+    }
+);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.use('/', indexRouter);
 
 app.listen(3000, () => console.log('app listening on port 3000!'));
