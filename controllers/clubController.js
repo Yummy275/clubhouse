368b3614394490a5545d8175721b22f0c5192db9
Yummy275 <htgonzalez94@yahoo.com>
2022-06-14 14:33:11 -0700

    club creation addds creator to clubs members

--- controllers/clubController.js
+++ controllers/clubController.js
@@ -3,7 +3,7 @@ const Club = require('../models/club');
 exports.createClub = (req, res, err) => {
     const club = new Club({
         title: req.body.title,
-        members: [],
+        members: [req.user.username],
         posts: [],
     }).save((err) => {
         if (err) {
